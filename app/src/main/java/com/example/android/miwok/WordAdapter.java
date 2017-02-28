aff7b4de5945fbab749d7f1ca377fb8fa6212e98
Yuriy Klyuyko <Yuriy Klyuyko>
2017-02-28 16:32:27 +0200

    2.6 Create WordAdapter to display a list of Word objects

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,63 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static android.R.attr.resource;
+
+/**
+ *  Custom ArrayAdapter that provide the layout for each list item based on the list of objects Word
+ */
+
+public class WordAdapter  extends ArrayAdapter<Word> {
+
+
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item,
+                    parent,
+                    false
+            );
+        }
+
+        // Get the object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID miwok_text_view
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getEnglishTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID english_text_view
+        TextView englishTextView = (TextView) listItemView.findViewById(R.id.english_text_view);
+        englishTextView.setText(currentWord.getmMiwokTranslation());
+
+        return listItemView;
+    }
+}
