78a24361f82afb1e436cd3c01b045f0ee7a539c3
Yuriy Klyuyko <Yuriy Klyuyko>
2017-03-01 10:37:59 +0200

    3.2 Modify Word class to store image resource ID

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,11 +5,15 @@ package com.example.android.miwok;
  */
 
 public class Word {
-    //  English translation for the word
+    // English translation for the word
     private String mEnglishTranslation;
 
-    //  Miwok translation for the word
+    // Miwok translation for the word
     private String mMiwokTranslation;
+
+    // Corresponding image to the word
+    private int mImageId;
+
 
     // default constructor, creates a new Word object.
     public Word(String englishTranslation, String miwokTranslation) {
@@ -17,11 +21,22 @@ public class Word {
         mMiwokTranslation = miwokTranslation;
     }
 
+    public Word(String englishTranslation, String miwokTranslation, int imageId) {
+        mEnglishTranslation = englishTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageId = imageId;
+    }
+
+
     public String getEnglishTranslation() {
         return mEnglishTranslation;
     }
 
-    public String getmMiwokTranslation() {
+    public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageId;
     }
 }
